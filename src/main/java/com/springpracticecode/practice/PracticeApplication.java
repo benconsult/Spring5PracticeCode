1964731ec88183edb79d0866f900b3506e6f8fa1
Aremu Benjamen <bendayo38@gmail.com>
2019-03-06 18:10:27 -0800

    adding multiple properties. closes #3

--- src/main/java/com/springpracticecode/practice/PracticeApplication.java
+++ src/main/java/com/springpracticecode/practice/PracticeApplication.java
@@ -1,6 +1,7 @@
 package com.springpracticecode.practice;
 
 import com.springpracticecode.practice.examplebeans.FakeDatasource;
+import com.springpracticecode.practice.examplebeans.FakeJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +14,9 @@ public class PracticeApplication {
         ApplicationContext ctx = SpringApplication.run(PracticeApplication.class, args);
         FakeDatasource fakeDatasource = (FakeDatasource) ctx.getBean(FakeDatasource.class);
         System.out.println(fakeDatasource.getUser());
+
+        FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+        System.out.println(fakeJmsBroker.getUsername());
     }
 
 }
--- src/main/java/com/springpracticecode/practice/config/PropertyConfig.java
+++ src/main/java/com/springpracticecode/practice/config/PropertyConfig.java
@@ -1,16 +1,22 @@
 package com.springpracticecode.practice.config;
 
 import com.springpracticecode.practice.examplebeans.FakeDatasource;
+import com.springpracticecode.practice.examplebeans.FakeJmsBroker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
     @Autowired
     Environment env;
@@ -23,6 +29,12 @@ public class PropertyConfig {
 
     @Value("${ben.dburl}")
     public String url;
+    @Value("${ben.jms.username}")
+    String jmsUsername;
+    @Value("${ben.jms.password}")
+    String jmsPassword;
+    @Value("${ben.jms.url}")
+    String jmsUrl;
 
 
 @Bean
@@ -33,6 +45,15 @@ public FakeDatasource fakeDatasource(){
     fakeDatasource.setUrl(url);
     return fakeDatasource;
 }
+@Bean
+public FakeJmsBroker fakeJmsBroker(){
+    FakeJmsBroker fakeJmsBroker=new FakeJmsBroker();
+    fakeJmsBroker.setUsername(jmsUsername);
+    fakeJmsBroker.setPassword(jmsPassword);
+    fakeJmsBroker.setUrl(jmsUrl);
+    return fakeJmsBroker;
+}
+
 @Bean
 public static PropertySourcesPlaceholderConfigurer properties(){
     PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer=new PropertySourcesPlaceholderConfigurer();
